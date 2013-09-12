c20af4820829f7af05279e4af7cac7d482673f02
Simon Traels Ravn <cs2@cs2.dk>
2013-09-12 12:08:06 +0000

    Actually save the order when adding the sales rep!

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -4,6 +4,7 @@ Spree::Order.class_eval do
   alias_method :orig_finalize!,  :finalize! unless method_defined? :orig_finalize!
   def finalize!
     sales_rep = user.sales_rep unless user.nil? or user.sales_rep.nil?
+    save
     orig_finalize!
   end
 end
