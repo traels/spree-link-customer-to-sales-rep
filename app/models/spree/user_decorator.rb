0e8b2ccb79df77f2390e3cbe849aff0bb63e41ad
Simon Traels Ravn <cs2@cs2.dk>
2013-12-12 10:42:53 +0000

    Prepare for 2.1 compatibility

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,5 +1,5 @@
 Spree::User.class_eval do
-  attr_accessible :sales_rep_id
+#  attr_accessible :sales_rep_id
   has_many :customers, :class_name => "Spree::User", :foreign_key => "sales_rep_id"
   belongs_to :sales_rep, :class_name => "Spree::User"
 end
